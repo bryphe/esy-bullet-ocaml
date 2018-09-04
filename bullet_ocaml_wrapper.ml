cb7566a32d93113d495e6b9703bd5f38ef82f48e
Bryan Phelps <bryphe@outlook.com>
2018-09-03 22:39:58 -0700

    Update wrapper

--- bullet_ocaml_wrapper.ml
+++ bullet_ocaml_wrapper.ml
@@ -1,6 +1,8 @@
 type world
 external bulletCreateDynamicsWorld: unit -> world = "bulletCreateDynamicsWorld"
+external bulletDestroyDynamicsWorld: world -> unit = "bulletDestroyDynamicsWorld"
 
 let () =
     let world = bulletCreateDynamicsWorld () in
-    print_endline ("hello")
+    print_endline ("hello");
+    (bulletDestroyDynamicsWorld world);
